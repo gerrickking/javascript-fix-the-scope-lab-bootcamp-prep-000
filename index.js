a2e981b5987298d1230e860574ae47e681c6a963
Gerrick King <gerrickkingfitness@gmail.com>
2018-02-11 21:21:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,12 @@ function yourAnimal() {
   return animal
 }
 
-
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
   const two = 2
 }
-
 
 var funkyFunction = function() {
   return function() {
@@ -29,4 +27,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var 'theFunk' = funkyFunction( )
+var theFunk = funkyFunction( )
